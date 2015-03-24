2678f67d2ce40d21df488772971f3c3228ee906e
Edwin Ortiz <eortiz@udel.edu>
2015-03-23 22:48:12 -0400

    testing works1

--- Lab3/src/poker/PokerJUnitTest.java
+++ Lab3/src/poker/PokerJUnitTest.java
@@ -2,63 +2,67 @@ package poker;
 
 import static org.junit.Assert.*;
 
-
 import java.util.ArrayList;
 
 import org.junit.Test;
 
-
-//FiveOfAKind(110),
-//RoyalFlush(100),
-//StraightFlush(90),
-//FourOfAKind(80),
-//FullHouse(70),
-//Flush(60),
-//Straight(50),
-//ThreeOfAKind(40),
-//TwoPair(30),
-//Pair(20),
-//HighCard(10);
-
 public class PokerJUnitTest {
 	@Test
 	public void testNaturalRoyalFlush() {
-		ArrayList<Card> SH = new ArrayList<Card>(); 
+		ArrayList<Card> SH = new ArrayList<Card>();
 		SH.add(new Card(eSuit.CLUBS, eRank.ACE));
 		SH.add(new Card(eSuit.CLUBS, eRank.KING));
 		SH.add(new Card(eSuit.CLUBS, eRank.QUEEN));
 		SH.add(new Card(eSuit.CLUBS, eRank.JACK));
 		SH.add(new Card(eSuit.CLUBS, eRank.TEN));
- 		
- 		Hand h = Hand.EvalHand(SH); 		
-		assertEquals("Expected NaturalRoyalFlush, but returned somethine else;", 120, h.getHandStrength());
+
+		Hand h = Hand.EvalHand(SH);
+		assertEquals(
+				"Expected NaturalRoyalFlush, but returned somethine else;",
+				120, h.getHandStrength());
 	}
-	
+
 	@Test
 	public void testFiveOfaKind() {
-		ArrayList<Card> SH = new ArrayList<Card>(); 
+		ArrayList<Card> SH = new ArrayList<Card>();
 		SH.add(new Card(eSuit.CLUBS, eRank.ACE));
 		SH.add(new Card(eSuit.CLUBS, eRank.ACE));
 		SH.add(new Card(eSuit.CLUBS, eRank.ACE));
 		SH.add(new Card(eSuit.CLUBS, eRank.ACE));
 		SH.add(new Card(eSuit.CLUBS, eRank.ACE));
- 		
- 		Hand h = Hand.EvalHand(SH);
-		assertEquals("Expected FiveOfAKind, but returned somethine else;", 110, h.getHandStrength());
+
+		Hand h = Hand.EvalHand(SH);
+		assertEquals("Expected FiveOfAKind, but returned somethine else;", 110,
+				h.getHandStrength());
 	}
-	
+
 	@Test
 	public void testRoyalFlush() {
-		ArrayList<Card> SH = new ArrayList<Card>(); 
+		ArrayList<Card> SH = new ArrayList<Card>();
 		SH.add(new Card(eSuit.JOKER, eRank.JOKER));
 		SH.add(new Card(eSuit.CLUBS, eRank.KING));
 		SH.add(new Card(eSuit.CLUBS, eRank.QUEEN));
 		SH.add(new Card(eSuit.CLUBS, eRank.JACK));
 		SH.add(new Card(eSuit.CLUBS, eRank.TEN));
-		Hand h = Hand.EvalHand(SH); 
-		assertEquals("Expected RoyalFlush, but returned somethine else;", 100, h.getHandStrength());
+		Hand h = Hand.EvalHand(SH);
+		assertEquals("Expected RoyalFlush, but returned somethine else;", 100,
+				h.getHandStrength());
 	}
- 	
+
+	@Test
+	public void testStraightFlush() {
+		ArrayList<Card> SH = new ArrayList<Card>();
+		SH.add(new Card(eSuit.CLUBS, eRank.THREE));
+		SH.add(new Card(eSuit.CLUBS, eRank.FOUR));
+		SH.add(new Card(eSuit.CLUBS, eRank.FIVE));
+		SH.add(new Card(eSuit.CLUBS, eRank.SIX));
+		SH.add(new Card(eSuit.CLUBS, eRank.SEVEN));
+
+		Hand h = Hand.EvalHand(SH);
+		assertEquals("Expected StraightFlush, but returned somethine else;",
+				90, h.getHandStrength());
+	}
+
 	@Test
 	public void testFourOfaKind() {
 		ArrayList<Card> SH = new ArrayList<Card>();
@@ -73,4 +77,77 @@ public class PokerJUnitTest {
 				h.getHandStrength());
 	}
 	
+	@Test
+	public void testFullHouse() {
+		ArrayList<Card> SH = new ArrayList<Card>();
+		SH.add(new Card(eSuit.CLUBS, eRank.ACE));
+		SH.add(new Card(eSuit.CLUBS, eRank.ACE));
+		SH.add(new Card(eSuit.CLUBS, eRank.ACE));
+		SH.add(new Card(eSuit.CLUBS, eRank.EIGHT));
+		SH.add(new Card(eSuit.CLUBS, eRank.EIGHT));
+
+		Hand h = Hand.EvalHand(SH);
+		assertEquals("Expected FullHouse, but returned somethine else;", 70,
+				h.getHandStrength());
+	}
+	
+	@Test
+	public void testFlush() {
+		ArrayList<Card> SH = new ArrayList<Card>();
+		SH.add(new Card(eSuit.CLUBS, eRank.THREE));
+		SH.add(new Card(eSuit.CLUBS, eRank.KING));
+		SH.add(new Card(eSuit.CLUBS, eRank.ACE));
+		SH.add(new Card(eSuit.CLUBS, eRank.TEN));
+		SH.add(new Card(eSuit.CLUBS, eRank.EIGHT));
+
+		Hand h = Hand.EvalHand(SH);
+		assertEquals("Expected Flush, but returned somethine else;", 60,
+				h.getHandStrength());
+	}
+	
+	@Test
+	public void testStraight() {
+		ArrayList<Card> SH = new ArrayList<Card>();
+		SH.add(new Card(eSuit.SPADES, eRank.SIX));
+		SH.add(new Card(eSuit.SPADES, eRank.FIVE));
+		SH.add(new Card(eSuit.CLUBS, eRank.FOUR));
+		SH.add(new Card(eSuit.CLUBS, eRank.THREE));
+		SH.add(new Card(eSuit.HEARTS, eRank.TWO));
+
+		Hand h = Hand.EvalHand(SH);
+		assertEquals("Expected Straight, but returned somethine else;", 50,
+				h.getHandStrength());
+	}
+
+	@Test
+	public void testThreeOfAKind() {
+		ArrayList<Card> SH = new ArrayList<Card>();
+		SH.add(new Card(eSuit.SPADES, eRank.SIX));
+		SH.add(new Card(eSuit.SPADES, eRank.SIX));
+		SH.add(new Card(eSuit.CLUBS, eRank.SIX));
+		SH.add(new Card(eSuit.CLUBS, eRank.THREE));
+		SH.add(new Card(eSuit.HEARTS, eRank.TWO));
+
+		Hand h = Hand.EvalHand(SH);
+		assertEquals("Expected ThreeOfAKind, but returned somethine else;", 40,
+				h.getHandStrength());
+	}
+
+	@Test
+	public void testTwoPair() {
+		ArrayList<Card> SH = new ArrayList<Card>();
+		SH.add(new Card(eSuit.SPADES, eRank.SIX));
+		SH.add(new Card(eSuit.SPADES, eRank.SIX));
+		SH.add(new Card(eSuit.CLUBS, eRank.EIGHT));
+		SH.add(new Card(eSuit.CLUBS, eRank.EIGHT));
+		SH.add(new Card(eSuit.HEARTS, eRank.TWO));
+
+		Hand h = Hand.EvalHand(SH);
+		assertEquals("Expected TwoPair, but returned somethine else;", 30,
+				h.getHandStrength());
+	}
+
+	//Pair(20),
+	//HighCard(10);
+
 }
